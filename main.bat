6eb4c393214f0dad2666e8e358437d5cae6985c5
CubeFun <109980144+CubeFun@users.noreply.github.com>
2023-09-09 22:10:30 +0200

    Add files via upload

--- main.bat
+++ main.bat
@@ -3,11 +3,11 @@
 msg * Chrome was installed incorrectly! Press OK to fix Chrome
 msg * Chrome will now download the Google Utility Tool to repair Google Chrome
 timeout 8
-C:\Users\Katika\Desktop\fakevirus\i.vbs
+- put path to i.vbs here -
 msg * Google Utility Tool was installed succesfully! 
 msg * Repairing Google Chrome via Google Utility Tool...
 timeout 4
-C:\Users\Katika\Desktop\fakevirus\s.vbs
+- put path to s.vbs here -
 msg * Google Chrome was repaired succesfully!
 timeout 8
 - put file path here -
